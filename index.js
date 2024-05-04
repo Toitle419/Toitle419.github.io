f8023bb2ed4bd3478a0670aa33ceaad49ba828ee
Toitle419 <117880942+Toitle419@users.noreply.github.com>
2024-05-04 22:04:39 +1000

    Added random background button

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-document.getElementById("p1").innerText = "What is this?"
-document.getElementById("main").setAttribute("styel", "color:purple")
+function changeBackgroundColour() {
+    let colour = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")"
+    document.getElementById("main").style.backgroundColor = colour
+}
