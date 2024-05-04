df958b11c3f97f1d09472246a5e0101a0960354e
Toitle419 <117880942+Toitle419@users.noreply.github.com>
2024-05-05 09:25:15 +1000

    Added clock

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function changeBackgroundColour() {
     let colour = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")"
     document.getElementById("main").style.backgroundColor = colour
 }
+function updateClock() {
+    const now = new Date()
+    let 
+    h = now.getHours(),
+    m = now.getMinutes(),
+    s = now.getSeconds()
+    if (m < 10) {m = "0" + m}
+    if (s < 10) {s = "0" + s}
+    let date = h + ":" + m + ":" + s
+    document.getElementById("clock").innerText = date
+}
+updateClock()
+setInterval(updateClock, 1000)
